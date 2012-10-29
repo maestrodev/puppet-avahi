52d3e42b49215f75a8062cafd379f3ea296ec595
Carlos Sanchez <csanchez@maestrodev.com>
2012-10-29 20:43:34 +0100

    Ensure messagebus is running and optionally manage the firewall

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,34 @@
-class avahi {
+# Start avahi services for mdns
+
+class avahi($firewall=false) {
   
-  $packages = ["avahi", "avahi-autoipd", "avahi-compat-libdns_sd", "avahi-glib", "avahi-gobject", "avahi-tools", "nss-mdns"]
+  $packages = ['avahi', 'avahi-autoipd', 'avahi-compat-libdns_sd', 'avahi-glib', 'avahi-gobject', 'avahi-tools', 'nss-mdns']
   
   package { $packages:
     ensure => present,
-    before => Service["avahi-daemon"],
-  } 
+    before => [Service['messagebus'],Service['avahi-daemon']],
+  }
  
-  service { "avahi-daemon":
-    hasstatus => true,
-    hasrestart => true,
+  service { 'messagebus':
+    enable => true,
+    ensure => running,
+  } ~>
+  service { 'avahi-daemon':
+    enable => true,
     ensure => running,
   }
-  
+
+  if $firewall {
+
+    # iptables -A INPUT -p udp --dport 5353 -d 224.0.0.251 -j ACCEPT
+    # iptables -A OUTPUT -p udp --dport 5353 -d 224.0.0.251 -j ACCEPT
+
+    firewall { '100 allow avahi':
+      chain       => ['INPUT', 'OUTPUT'],
+      proto       => 'udp',
+      dport       => '5353',
+      destination => '224.0.0.251',
+      action      => 'accept',
+    }
+  }
 }
